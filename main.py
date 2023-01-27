8329711336f1cf031fb701a3f5d65db5b75dbcfb
sadamccr <sadamccr@gmail.com>
2023-01-27 17:24:17 +0500

    node created

---
+++ main.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.previous = None
+
+
+nodeOne = Node('Sadam')
+nodeTwo = Node('Hamza')
+nodeThree = Node('Asad')
