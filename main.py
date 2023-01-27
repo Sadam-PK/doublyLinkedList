a5005a86298ce118e76f1473a297034d3ce33326
sadamccr <sadamccr@gmail.com>
2023-01-27 17:32:06 +0500

    insert at end done

--- main.py
+++ main.py
@@ -5,18 +5,26 @@ class Node:
         self.previous = None
 
 
-nodeOne = Node('Sadam')
-nodeTwo = Node('Hamza')
-nodeThree = Node('Asad')
-
-
 class LinkedList:
     def __init__(self):
         self.head = None
 
-    def insert(self, node):
-        pass
+    # sadam - > none
+    def insert(self, new_node):
+        if self.head is None:
+            self.head = new_node
+            return
+        current_node = self.head
+        while True:
+            if current_node.next is None:
+                break
+            current_node = current_node.next
+            new_node.previous = current_node
 
+
+nodeOne = Node('Sadam')
+nodeTwo = Node('Hamza')
+nodeThree = Node('Asad')
 
 linKedList = LinkedList()
 linKedList.insert(nodeOne)
